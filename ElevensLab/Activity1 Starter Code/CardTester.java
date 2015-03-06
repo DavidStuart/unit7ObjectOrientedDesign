c6abca038d646d941d504798446124a72eb82fee
DavidStuart <davidstuart31@gmail.com>
2015-03-06 15:01:31 -0600

    Stuff
    FINISH THIS IDIOT

--- ElevensLab/Activity1 Starter Code/CardTester.java
+++ ElevensLab/Activity1 Starter Code/CardTester.java
@@ -3,11 +3,17 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        Card card1 = new Card("Ace","Spade",14);
+        Card card2 = new Card("Ace","Spade",14);
+        System.out.println(card1.toString());
+        if(card1.matches(card2))
+        {
+            System.out.println("card 1 is equal to card 2.");
+        }
+    }
 }
